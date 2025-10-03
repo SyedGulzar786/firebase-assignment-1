d56b3678364595b8616e904e0cabc217ddc55e40
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-10-03 05:14:37 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBQjlm6ZK-6gAahwN_fgon8zxtSuKOOtS0",
+    authDomain: "fir-assognment-1.firebaseapp.com",
+    projectId: "fir-assognment-1",
+    storageBucket: "fir-assognment-1.firebasestorage.app",
+    messagingSenderId: "217878892831",
+    appId: "1:217878892831:web:fbc75a91e56cc49f7c602a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
